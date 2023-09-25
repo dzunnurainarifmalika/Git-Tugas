20b45e2a937db4a432b2c5d4f40c9fcb2b04b49b
dzunnurainarifmalika <113917569+dzunnurainarifmalika@users.noreply.github.com>
2023-09-25 14:38:30 +0700

    Add files via upload

---
+++ tebak_nomor.py
@@ -0,0 +1,28 @@
+import random
+
+# Fungsi untuk memulai permainan
+def guess_the_number():
+    # Memilih angka acak antara 1 dan 100
+    target_number = random.randint(1, 100)
+    attempts = 0
+
+    print("Selamat datang di Permainan Tebak Angka!")
+    print("Saya telah memilih angka antara 1 dan 100. Coba tebak!")
+
+    while True:
+        try:
+            guess = int(input("Masukkan tebakan Anda: "))
+            attempts += 1
+
+            if guess < target_number:
+                print("Tebakan Anda terlalu kecil. Coba lagi.")
+            elif guess > target_number:
+                print("Tebakan Anda terlalu besar. Coba lagi.")
+            else:
+                print(f"Selamat! Anda menebak angka {target_number} dengan benar dalam {attempts} percobaan.")
+                break
+        except ValueError:
+            print("Masukkan harus berupa angka.")
+
+if __name__ == "__main__":
+    guess_the_number()
